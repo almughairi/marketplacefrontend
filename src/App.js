180708db18f49696663a4cace385112680121161
almughairi <132788151+almughairi@users.noreply.github.com>
2023-05-09 08:51:37 +0000

    Adding variables

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 
 function App() {
-  return (
 
-    
+  const departmentName ='Digital Department'
+  return (
     <div className="container">
       <img src="Angelo_Gemmi_rosette_3.png"/>
 
@@ -22,10 +22,10 @@ function App() {
           </ul>
 
 
-   
+   <article>
       <div className="container">
       <p>
-      Alizz islamic bank <b>digital department</b> is a leading provider
+      Alizz islamic bank <b>{departmentName}</b> is a leading provider
        of comprehensive digital solutions, offering
       a wide range of services that help businesses achieve 
       their goals in the digital world. With a team of highly skilled 
@@ -33,8 +33,7 @@ function App() {
       management, network support, and business technology support. We work closely with clients to understand their unique needs and provide customized strategies that drive growth and success. Our team is dedicated to delivering results that exceed our clients' expectations, leveraging cutting-edge technology and innovative solutions. Whether it's managing complex services, optimizing transactions, or providing expert network and technology support, our digital department is here to help businesses thrive in today's digital landscape."
       </p>
       </div>
-    
-
+      </article>
     </div>
   );
 }
