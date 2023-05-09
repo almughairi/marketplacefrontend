6621bf15dc0c990ca76c4d34824b3168e701fa70
Salim almughairi <salimalmughairisoftware@gmail.com>
2023-05-09 10:19:02 +0400

    the initial Digital frontend

--- src/App.js
+++ src/App.js
@@ -2,26 +2,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+
+    
+    <div className="container">
+          <div className="login">
+          <ul>
+              <li>Login</li>
+              <li>Email Support</li>
+              <li>Telephone Support</li>
+          </ul>
+          </div>
+          <h1></h1>
+          <ul>
+            <li>Support Document</li>
+            <li>System Infrustructure </li>
+            <li>Design Documents</li>  
+          </ul>
     </div>
   );
 }
